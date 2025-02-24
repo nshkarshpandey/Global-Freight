0608e0f1e36defdf2e83c509795acc1d2836d2b7
nshkarshpandey <nishkarsh0886.be20@chitkara.edu.in>
2025-02-25 04:04:47 +0530

    freight txt section added-2

--- global_freight/src/components/freight.tsx
+++ global_freight/src/components/freight.tsx
@@ -8,11 +8,12 @@ export default function Freight() {
            Freight Forwarders Act As Intermediaries Between Shippers And Various transportation Services, Such As Shipping Lines, Airlines, Trucking Companies, And Railroads.
         </p>
       </div>
-      <div className="w-full md:w-1/2 p-4">
-        <p className="text-3xl font-bold text-gray-800 mb-4">
+      <div className="w-full md:w-1/2 p-4 relative fri-txt-2">
+        <p className="text-3xl font-bold text-gray-800 mb-4 ">
           Freight forwarders arrange and manage goods <span className='text-[#ec7620]'>transportation</span> for shippers
         </p>
       </div>
     </div>
   );
 }
+
