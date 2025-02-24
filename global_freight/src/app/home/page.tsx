bcc3610b2de29784cd1d0eaa50f1922ed48fcc7d
nshkarshpandey <nishkarsh0886.be20@chitkara.edu.in>
2025-02-25 03:49:07 +0530

    freight txt section added

--- global_freight/src/app/home/page.tsx
+++ global_freight/src/app/home/page.tsx
@@ -4,16 +4,16 @@ import Header from "@/components/header";
 import Hero from "@/components/hero";
 import { Container } from "@/components/container";
 import Service from "@/components/service";
-import useScreenSize from '@/hooks/useScreenSize';
+// import useScreenSize from '@/hooks/useScreenSize';
 import Freight from "@/components/freight";
 // import Image from "next/image";
 
 export default function Home() {
-   const { width, height } = useScreenSize();
+  //  const { width, height } = useScreenSize();
   return (
     
     <Container>
-      <p className="relative text-lg text-black">Current Screen Size: {width}px X {height}px</p>
+      {/* <p className="relative text-lg text-black">Current Screen Size: {width}px X {height}px</p> */}
       <Header />
       <Hero />
       <Service />
