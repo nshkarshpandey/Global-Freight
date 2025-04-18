977f25c1c96697fd454373552adc1519401c90f5
nshkarshpandey <nishkarsh0886.be20@chitkara.edu.in>
2025-04-19 03:30:25 +0530

    test-2

--- global_freight/src/components/custom.tsx
+++ global_freight/src/components/custom.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 export default function CustomsClearance() {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
